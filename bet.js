84f1cad9dc2bd5763b9da6c56d4e50fcd3974616
victorbraga <victor.medeiros@contraprova.com.br>
2023-01-04 10:24:54 -0300

    Finalização de Montagem de Objeto Match

--- bet.js
+++ bet.js
@@ -6,7 +6,7 @@ async function getInfo(){
     const matchList = [];
     const linksList = [];
 
-    const browser = await pup.launch({headless: false});
+    const browser = await pup.launch({headless: true});
     const page = await browser.newPage();
     
     await page.goto(url);
@@ -32,52 +32,46 @@ async function getInfo(){
     })
     let id = 0;
     for(const links of linksList){       
-        // if(id == 1) continue; 
+        if(id == 20) continue; 
         await page.goto(links);
         await page.waitForSelector('#team_stats_extra');
 
-        // const teamCards = await page.$$eval('.cards > span' , elCard=> elCard.map(teamCard => teamCard));   
-        // console.log(teamCards[0]);
-
-        // A formação do yellow card - red card e do yellowRed card esta errada, tanto A quanto B
-
-        const teamACards = await page.$$eval('div > .a > div > .yellow_card', yellowCardsA=> yellowCardsA.map(cardsA => cardsA));
-        const teamBCards = await page.$$eval('div > .b > div > .yellow_card', yellowCardsB=> yellowCardsB.map(cardsB => cardsB));
-
-        const teamARedCards = await page.$$eval('div > .a > div > .red_card', yellowCardsA=> yellowCardsA.map(cardsA => cardsA));
-        const teamBRedCards = await page.$$eval('div > .b > div > .red_card', yellowCardsB=> yellowCardsB.map(cardsB => cardsB));
-
-        const teamAyellowRedCard = await page.$$eval('div > .a > div > .yellow_red_card', yellowCardsA=> yellowCardsA.map(cardsA => cardsA));
-        const teamByellowRedCard = await page.$$eval('div > .b > div > .yellow_red_card', yellowCardsB=> yellowCardsB.map(cardsB => cardsB));
+        const teamAYellowCards = await page.$$eval('div > .a > div > .yellow_card', yellowCardsA=> yellowCardsA.map(cardsA => cardsA));
+        const teamBYellowCards = await page.$$eval('div > .b > div > .yellow_card', yellowCardsB=> yellowCardsB.map(cardsB => cardsB));
         
+        const teamARedCards = await page.$$eval('div > .a > div > .red_card', yellowCardsA=> yellowCardsA.map(cardsA => cardsA));        
+        const teamBRedCards = await page.$$eval('div > .b > div > .red_card', yellowCardsB=> yellowCardsB.map(cardsB => cardsB));        
+
+        const teamAyellowRedCard = await page.$$eval('div > .a > div > .yellow_red_card', yellowCardsA=> yellowCardsA.map(cardsA => cardsA));        
+        const teamByellowRedCard = await page.$$eval('div > .b > div > .yellow_red_card', yellowCardsB=> yellowCardsB.map(cardsB => cardsB));        
+
         if(teamARedCards && teamAyellowRedCard){
-            redCardsTeamA = Number(teamARedCards.length)+Number(teamAyellowRedCard.length);
+            redCardsTeamA = Number(teamARedCards.length) + Number(teamAyellowRedCard.length);            
         }
         if(teamARedCards && !teamAyellowRedCard){
-            redCardsTeamA = Number(teamARedCards.length);
-        }
+            redCardsTeamA = Number(teamARedCards.length);            
+        }        
         if(!teamARedCards && teamAyellowRedCard){
-            redCardsTeamA = Number(teamAyellowRedCard.length);
-        }
+            redCardsTeamA = Number(teamAyellowRedCard.length);            
+        }        
         if(!teamARedCards && !teamAyellowRedCard){
-            redCardsTeamA = Number(0);
+            redCardsTeamA = Number(0);            
         }
-
+                
         if(teamBRedCards && teamByellowRedCard){
-            redCardsTeamB = Number(teamBRedCards.length)+Number(teamByellowRedCard.length);
+            redCardsTeamB = Number(teamBRedCards.length) + Number(teamByellowRedCard.length);            
         }
         if(teamBRedCards && !teamByellowRedCard){
-            redCardsTeamB = Number(teamBRedCards.length);
-        }
+            redCardsTeamB = Number(teamBRedCards.length);            
+        }        
         if(!teamBRedCards && teamByellowRedCard){
-            redCardsTeamB = Number(teamByellowRedCard.length);
-        }
+            redCardsTeamB = Number(teamByellowRedCard.length);            
+        }        
         if(!teamBRedCards && !teamByellowRedCard){
-            redCardsTeamB = Number(0);
-        }
-        
+            redCardsTeamB = Number(0);            
+        }    
+                       
         const teamStats = await page.$$eval('#team_stats_extra > div ' , el=> el.map(team => team.innerText));   
-
         const matchStringJoin = [teamStats[0], teamStats[1],teamStats[2]].join('\n');        
         const matchString = matchStringJoin.split("\n");
                 
@@ -85,7 +79,7 @@ async function getInfo(){
             id: id,
             teamA:{
                 name:matchString[0],
-                yellowCard: teamACards.length,
+                yellowCard: teamAYellowCards.length,
                 redCard: redCardsTeamA,
                 fouls: matchString[3],
                 cornerKick: matchString[6],
@@ -98,7 +92,7 @@ async function getInfo(){
             },
             teamB:{
                 name:matchString[2],      
-                yellowCard: teamBCards.length,
+                yellowCard: teamBYellowCards.length,
                 redCard: redCardsTeamB,
                 fouls: matchString[5],
                 cornerKick: matchString[8],
@@ -111,6 +105,7 @@ async function getInfo(){
         }  
         
         matchList.push(match);
+        console.log(match);
         id ++;                
     }
 
@@ -119,11 +114,11 @@ async function getInfo(){
     return matchList;    
 }
 
-(async ()=>{
-    const temp = await getInfo();   
-    console.log(temp);     
-})();
+// (async ()=>{
+//     const temp = await getInfo();   
+//     console.log(temp);     
+// })();
 
-// getInfo(); 
+getInfo(); 
 
 
